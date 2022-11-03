92d5b25deba78c1f15b21f91ce8a580ee27e9433
yskim <yskimweb@gmail.com>
2022-11-03 22:59:41 +0900

    init  : login
    s

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-main class="blue-grey lighten-4">
+      <!-- Login Component -->
+      <v-container style="max-width: 450px" fill-height>
+        <v-layout align-center row wrap>
+          <v-flex xs12>
+            <router-view />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+  components: {},
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card>
+    <div class="pa-10">
+      <h1 style="text-align: center" class="mb-10">Login</h1>
+      <form>
+        <v-text-field
+          label="ID"
+          prepend-inner-icon="mdi-account"
+        ></v-text-field>
+        <v-text-field
+          prepend-inner-icon="mdi-lock"
+          type="password"
+          label="Password"
+        >
+        </v-text-field>
+        <v-btn
+          type="submit"
+          color="blue lighten-1 text-capitalize"
+          depressed
+          large
+          block
+          dark
+          class="mb-3"
+        >
+          Login
+        </v-btn>
+        <v-btn
+          @click="addUserShow"
+          color="blue lighten-1 text-capitalize"
+          depressed
+          large
+          block
+          dark
+        >
+          Sign Up
+        </v-btn>
+      </form>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  methods: {},
+};
+</script>
