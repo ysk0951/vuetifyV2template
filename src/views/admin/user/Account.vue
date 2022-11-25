8a39f82182d1700f8959517f87c08c1331a18deb
yskim <yskimweb@gmail.com>
2022-11-25 21:12:56 +0900

    feat : 신규생성 modal

--- src/views/admin/user/Account.vue
+++ src/views/admin/user/Account.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
+    <SetDialog ref="add">
+      <AddAcount />
+    </SetDialog>
     <h3 class="mt-4 mb-2">아이디 관리</h3>
     <hr class="mb-4" />
     <div class="service">
       <div class="filter">
         <v-row class="row">
-          <v-col cols="12" sm="3">
+          <v-col cols="11" sm="3">
             <h4>계정 구분</h4>
             <v-select
               :items="accountType"
               :item-text="'text'"
               :item-value="'key'"
               v-model="type"
+              outlined
+              :id="'account'"
             ></v-select>
           </v-col>
-          <v-col cols="12" sm="1"></v-col>
-          <v-col cols="12" sm="3">
+          <v-col cols="11" sm="1"></v-col>
+          <v-col cols="11" sm="3">
             <h4>이름</h4>
             <v-text-field
               outlined
@@ -23,8 +28,8 @@
               placeholder="이름을 입력해주세요"
             ></v-text-field>
           </v-col>
-          <v-col cols="12" sm="1"></v-col>
-          <v-col cols="12" sm="4" style="padding-left: 0px">
+          <v-col cols="11" sm="1"></v-col>
+          <v-col cols="11" sm="3" style="padding-left: 0px">
             <h4>이메일</h4>
             <v-text-field
               outlined
@@ -34,7 +39,7 @@
           </v-col>
         </v-row>
         <v-row class="row mb-3">
-          <v-col cols="12" sm="3">
+          <v-col cols="11" sm="3">
             <h4>기업명</h4>
             <v-text-field
               outlined
@@ -42,23 +47,26 @@
               placeholder="기업명을 입력해주세요"
             ></v-text-field>
           </v-col>
-          <v-col cols="12" sm="1"></v-col>
-          <v-col cols="12" sm="3">
-            <h4>이름</h4>
+          <v-col cols="11" sm="1"></v-col>
+          <v-col cols="11" sm="3">
+            <h4>사번</h4>
             <v-text-field
               outlined
               dense
               placeholder="이름을 입력해주세요"
             ></v-text-field>
           </v-col>
-          <v-col cols="12" sm="1"></v-col>
-          <v-col cols="12" sm="4" style="padding-left: 0px">
-            <h4>이메일</h4>
-            <v-text-field
+          <v-col cols="11" sm="1"></v-col>
+          <v-col cols="11" sm="3" style="padding-left: 0px">
+            <h4>재직</h4>
+            <v-select
+              :items="accountType"
+              :item-text="'text'"
+              :item-value="'key'"
+              v-model="type"
               outlined
-              dense
-              placeholder="이메일 주소를 입력해주세요"
-            ></v-text-field>
+              id="work"
+            ></v-select>
           </v-col>
         </v-row>
       </div>
@@ -73,7 +81,12 @@
           </v-card-actions>
         </div>
       </div>
-      <h3 class="mt-4 mb-2">목록</h3>
+      <h3 class="mt-16 mb-2 pl-1 pr-1">
+        <div class="wrapperSpace">
+          목록
+          <v-btn depressed color="primary" @click="add">신규생성</v-btn>
+        </div>
+      </h3>
       <hr class="mb-4" />
       <Grid :setting="setting" />
     </div>
@@ -81,6 +94,9 @@
 </template>
 <script>
 import Grid from "@/components/Grid.vue";
+import SetDialog from "@/components/SetDialog.vue";
+import AddAcount from "@/views/admin/user/AddAcount.vue";
+import { mapMutations } from "vuex";
 export default {
   data() {
     return {
@@ -111,15 +127,33 @@ export default {
     };
   },
   methods: {
+    ...mapMutations("modal", [
+      "SET_DIALOG_TITLE",
+      "SET_DIALOG_TEXT",
+      "SET_HIGHT",
+      "SET_MAX_WIDTH",
+      "SET_MODAL",
+      "RESET_MODAL",
+    ]),
     reset() {},
     onApprove() {},
+    add() {
+      this.SET_MODAL({
+        height: 600,
+        width: 750,
+        closable: true,
+      });
+      this.$refs.add.openModal();
+    },
   },
   components: {
     Grid,
+    SetDialog,
+    AddAcount,
   },
 };
 </script>
-<style scoped>
+<style>
 .filter {
   /* background-color: wheat; */
   border-style: solid;
@@ -133,5 +167,23 @@ export default {
 .service {
   margin: auto;
   width: 70%;
+}
+
+.v-input__slot {
+  width: 266px !important;
+  height: 30px !important;
+}
+.v-select__slot > div.v-select__selections {
+  position: absolute;
+  top: 0;
+}
+div.v-input__slot > fieldset {
+  height: 46px !important;
+}
+
+div.v-select__slot > div.v-input__append-inner > div {
+  position: absolute;
+  top: 10px;
+  right: 0;
 }
 </style>
---
+++ src/views/admin/user/AddAcount.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="wrapper">
+    <div class="add">
+      <v-row>
+        <v-col cols="12" sm="6">
+          <h4>계정 구분</h4>
+          <v-select
+            :items="accountType"
+            :item-text="'text'"
+            :item-value="'key'"
+            v-model="type"
+            outlined
+            :id="'account'"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <h4>이름</h4>
+          <v-text-field
+            outlined
+            dense
+            placeholder="이름을 입력해주세요"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <h4>계정 구분</h4>
+          <v-select
+            :items="accountType"
+            :item-text="'text'"
+            :item-value="'key'"
+            v-model="type"
+            outlined
+            :id="'account'"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <h4>이름</h4>
+          <v-text-field
+            outlined
+            dense
+            placeholder="이름을 입력해주세요"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <h4>계정 구분</h4>
+          <v-select
+            :items="accountType"
+            :item-text="'text'"
+            :item-value="'key'"
+            v-model="type"
+            outlined
+            :id="'account'"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <h4>이름</h4>
+          <v-text-field
+            outlined
+            dense
+            placeholder="이름을 입력해주세요"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <h4>계정 구분</h4>
+          <v-select
+            :items="accountType"
+            :item-text="'text'"
+            :item-value="'key'"
+            v-model="type"
+            outlined
+            :id="'account'"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <h4>이름</h4>
+          <v-text-field
+            outlined
+            dense
+            placeholder="이름을 입력해주세요"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <h4>계정 구분</h4>
+          <v-select
+            :items="accountType"
+            :item-text="'text'"
+            :item-value="'key'"
+            v-model="type"
+            outlined
+            :id="'account'"
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <h4>이름</h4>
+          <v-text-field
+            outlined
+            dense
+            placeholder="이름을 입력해주세요"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      accountType: [
+        {
+          key: "all",
+          text: "전체",
+        },
+        {
+          key: "admin",
+          text: "관리자",
+        },
+        {
+          key: "user",
+          text: "회원",
+        },
+      ],
+      type: "all",
+      setting: {
+        gridName: "account",
+        columns: [],
+        fields: [],
+        existFavorite: false,
+        layout: undefined,
+        existCalendar: false,
+        existAddr: false,
+      },
+    };
+  },
+  methods: {},
+  components: {},
+};
+</script>
+<style>
+.add {
+  width: 88%;
+}
+</style>
