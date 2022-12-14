d1e23968b47c404fc1d0b9ad5e73e50b368b85f9
yskim <yskimweb@gmail.com>
2022-12-14 12:03:32 +0900

    feat : menstruumAdd

--- src/views/master/masterTap/MenstruumAdd.vue
+++ src/views/master/masterTap/MenstruumAdd.vue
@@ -1,20 +1,49 @@
 <template>
-  <div>
+  <div class="menstrumm">
     <h3 class="mt-4 mb-2">용매조성 마스터 등록</h3>
     <hr class="mb-4" />
-    <v-row v-for="(item, idx) in layout" :key="idx">
-      <v-col cols="12" sm="3">
-        <h4>{{ item.excelKey ? item.excelKey : item.fieldName | rm }}</h4>
-        <v-text-field
-          outlined
-          dense
-          placeholder="00.00/00.00/00.00/00.00/00.00/00.00/00.00/00.00"
-        ></v-text-field>
+    <v-row v-for="idx in row" :key="idx">
+      <v-col
+        cols="12"
+        :sm="item(idx, i) === '비고' ? 6 : 3"
+        v-for="i in col"
+        :key="i"
+      >
+        <template v-if="item(idx, i)">
+          <h4>{{ item(idx, i) }}</h4>
+          <template v-if="item(idx, i) === 'SUM'">
+            <v-text-field outlined dense disabled filled></v-text-field
+          ></template>
+          <template v-else-if="item(idx, i) === '비고'">
+            <v-text-field
+              outlined
+              dense
+              :placeholder="item(idx, i) + '입력'"
+              v-model="param[bindKey[(idx - 1) * col + i - 1]]"
+            ></v-text-field>
+          </template>
+          <template v-else-if="item(idx, i) === '밀도'">
+            <v-text-field
+              outlined
+              dense
+              :placeholder="item(idx, i) + '입력'"
+              v-model="param[bindKey[(idx - 1) * col + i - 1]]"
+            ></v-text-field>
+          </template>
+          <template v-else>
+            <v-text-field
+              outlined
+              dense
+              :placeholder="item(idx, i) + '입력'"
+              v-model="param[bindKey[(idx - 1) * col + i - 1]]"
+            ></v-text-field>
+          </template>
+        </template>
       </v-col>
     </v-row>
     <div class="wrapper">
       <v-card-actions>
-        <v-btn depressed @click="reset">초기화</v-btn>
+        <v-btn depressed @click="setData">초기화</v-btn>
       </v-card-actions>
       <v-card-actions>
         <v-btn depressed color="primary" @click="save">저장</v-btn>
@@ -30,15 +59,68 @@ export default {
     return {
       grid: "menstrummDetail",
       settings: menstrumm,
-      layout: menstrumm.fields,
+      layout: menstrumm.columns,
+      col: 4,
+      param: {},
+      bindKey: [],
     };
   },
   methods: {
     newSample() {
       this.$emit("newSample");
     },
-    save() {},
-    reset() {},
+    save() {
+      console.log(this.param);
+    },
+    item(idx, i) {
+      const index = (idx - 1) * this.col + i - 1;
+      if (index < this.layout.length) {
+        const ret = this.layout[index];
+        return ret.header.text;
+      } else {
+        return;
+      }
+    },
+    bindModel(idx, i) {
+      const index = (idx - 1) * this.col + i - 1;
+      const field = this.settings.fields[index].fieldName;
+      switch (field) {
+        case "SUM1":
+          return "sum";
+        case "비고1":
+          return "memo";
+        case "밀도1":
+          return "mildo";
+        default:
+          return field;
+      }
+    },
+    setData() {
+      const obj = {};
+      const key = [];
+      _.each(this.settings.fields, (v) => {
+        if (v.fieldName === "SUM1") {
+          key.push("sum");
+          obj.sum = "";
+        } else if (v.fieldName === "비고1") {
+          key.push("memo");
+          obj.memo = "";
+        } else if (v.fieldName === "밀도1") {
+          key.push("mildo");
+          obj.mildo = "";
+        } else {
+          key.push(v.fieldName);
+          obj[v.fieldName] = "";
+        }
+      });
+      this.bindKey = key;
+      this.param = obj;
+    },
+  },
+  computed: {
+    row() {
+      return Math.ceil(this.layout.length / this.col);
+    },
   },
   components: {},
   filters: {
@@ -51,12 +133,16 @@ export default {
       }
     },
   },
+  mounted() {
+    this.setData();
+    console.log(this.bindKey);
+  },
 };
 </script>
 <style lang="scss">
-.confirmSample {
-  .v-text-field__details {
-    display: none;
+.menstrumm {
+  .row {
+    margin-top: 0px;
   }
 }
 </style>
