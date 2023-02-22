ed42f37613183afe5d91f25fb1a2b97c18707b5b
yskim <yskimweb@gmail.com>
2023-02-22 15:35:35 +0900

    feat : main

---
+++ src/assets/grid/delivery.js
@@ -0,0 +1,41 @@
+import _ from "lodash";
+import { makeCell } from "./gridUtill";
+let fields = [];
+let columns = [];
+const data = [
+  {
+    field: "lotNo",
+    alias: "Lot No",
+  },
+  {
+    field: "Code Grade",
+    alias: "Code Grade",
+  },
+  {
+    field: "납기일",
+    alias: "납기일",
+  },
+  {
+    field: "qty",
+    alias: "제품수량(kg)",
+  },
+  {
+    field: "접수상태",
+    alias: "접수 상태",
+  },
+  {
+    field: "producestat",
+    alias: "제조 진행사항",
+  },
+  {
+    field: "producestat_",
+    alias: "출하 예정일",
+  },
+];
+
+_.each(data, function (o) {
+  o.indexExclusive = true;
+  makeCell(1, [o], fields, columns);
+});
+
+export { fields, columns };
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,17 +5,27 @@
       <div class="wrapper">
         <div class="filter">
           <v-row>
-            <v-col cols="12" sm="4">
+            <v-col cols="12" sm="2">
               <v-card elevation="3" @click="newRq"
                 >신규요청<span>{{ dash.new }}</span></v-card
               >
             </v-col>
-            <v-col cols="12" sm="4">
+            <v-col cols="12" sm="2">
               <v-card elevation="3" @click="progress"
                 >제조중<span>{{ dash.progress }}</span></v-card
               >
             </v-col>
-            <v-col cols="12" sm="4">
+            <v-col cols="12" sm="2">
+              <v-card elevation="3" @click="progressDelay"
+                >제조지연<span>{{ dash.new }}</span></v-card
+              >
+            </v-col>
+            <v-col cols="12" sm="2">
+              <v-card elevation="3" @click="deliveryDelay"
+                >출하지연<span>{{ dash.new }}</span></v-card
+              >
+            </v-col>
+            <v-col cols="12" sm="2">
               <v-card elevation="3" @click="delivery"
                 >출하예정 <span>{{ dash.outdue }}</span>
               </v-card>
@@ -31,18 +41,35 @@
             domName="new"
             ref="newGrid"
             :settings="newGrid"
+            @changeData="newGrid"
           />
           <RealGrid
             v-show="key === 'delivery'"
             domName="delivery"
             ref="deliveryGrid"
             :settings="deliveryGrid"
+            @changeData="delivery"
+          />
+          <RealGrid
+            v-show="key === 'progressDelay'"
+            domName="progressDelay"
+            ref="progressDelayGrid"
+            :settings="progressDelayGrid"
+            @changeData="progressDelay"
+          />
+          <RealGrid
+            v-show="key === 'deliveryDelay'"
+            domName="deliveryDelay"
+            ref="deliveryGridDelay"
+            :settings="deliveryGridDelay"
+            @changeData="deliveryDelay"
           />
           <RealGrid
             v-show="key === 'progress'"
             domName="progress"
             ref="progressGrid"
             :settings="progressGrid"
+            @changeData="progress"
           />
         </div>
       </div>
@@ -52,9 +79,9 @@
 <script>
 import SetDialog from "@/components/SetDialog";
 import RealGrid from "@/components/RealGrid.vue";
-import * as newGrid from "@/assets/grid/sampleRequest";
-import * as deliveryGrid from "@/assets/grid/sampleRequest";
-import * as progressGrid from "@/assets/grid/sampleRequest";
+import * as newGrid from "@/assets/grid/newGrid";
+import * as deliveryGrid from "@/assets/grid/delivery";
+import * as progressGrid from "@/assets/grid/progress";
 import _ from "lodash";
 import { mapState, mapMutations } from "vuex";
 import { dashcount, dashdelivery, dashnew, progress } from "api/sample/sample";
@@ -71,20 +98,23 @@ export default {
           value: "신규요청",
         },
         {
-          key: "delivery",
-          value: "출하예정",
-        },
-        {
           key: "progress",
           value: "제조중",
         },
+        {
+          key: "progressDelay",
+          value: "제조지연",
+        },
+        {
+          key: "deliveryDelay",
+          value: "출하지연",
+        },
+        {
+          key: "delivery",
+          value: "출하예정",
+        },
       ],
       dash: {},
-      gridData: {
-        new: [],
-        progress: [],
-        delivery: [],
-      },
       param: { currentPage: 1, pageSize: 10 },
     };
   },
@@ -117,37 +147,37 @@ export default {
   },
   methods: {
     ...mapMutations("menu", ["SET_MENU"]),
-    newRq() {
-      this.key = "new";
-      this.$refs.newGrid.loadData(this.gridData.new.items);
-      // this.$refs.newGrid.setPage(this.gridData.new.params);
+    async chageTab(v, key, ref, fun) {
+      this.key = key;
+      const res = await fun({
+        ...this.param,
+        currentPage: _.isNumber(v) ? v : 1,
+      });
+      const data = res.data.data;
+      const grid = this.$refs[ref];
+      if (grid) {
+        grid.loadData(data.items);
+        grid.setPage(data.params);
+      }
     },
-    delivery() {
-      this.key = "delivery";
-      this.$refs.deliveryGrid.loadData(this.gridData.delivery.items);
-      this.$refs.deliveryGrid.setPage(this.gridData.delivery.params);
+    newRq(v) {
+      this.chageTab(v, "new", "newGrid", dashnew);
     },
-    progress() {
-      this.key = "progress";
-      this.$refs.progressGrid.loadData(this.gridData.progress.items);
-      this.$refs.progressGrid.setPage(this.gridData.progress.params);
+    progress(v) {
+      this.chageTab(v, "progress", "progressGrid", progress);
     },
+    deliveryDelay() {},
+    delivery(v) {
+      this.chageTab(v, "delivery", "deliveryGrid", dashdelivery);
+    },
+    progressDelay() {},
     async loadData() {
       await this.loadDeshCount();
-      await this.loadDeashGridData();
-      this.newRq();
+      await this.newRq();
     },
     async loadDeshCount() {
       const res = await dashcount();
       this.dash = res.data.data;
-    },
-    async loadDeashGridData() {
-      const newDash = await dashnew(this.param);
-      const deliveryDash = await dashdelivery(this.param);
-      const progressDash = await progress(this.param);
-      this.gridData.delivery = deliveryDash.data.data;
-      this.gridData.progress = progressDash.data.data;
-      this.gridData.new = newDash.data.data;
     },
   },
   mounted() {
